0d7ae781b62f2e7ee27134b379af496c1db17002
Kevin Gomez Gonzalez <kev.gomez97@hotmail.com>
2018-09-25 16:48:08 -0500

    Servlet AccionesIntercambio.java

--- Intercambio_Regalos/src/java/intercambio/AccionesIntercambio.java
+++ Intercambio_Regalos/src/java/intercambio/AccionesIntercambio.java
@@ -2,7 +2,9 @@ package intercambio;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -22,14 +24,98 @@ public class AccionesIntercambio extends HttpServlet {
         try{
             conexion.crearConexion("localhost", "convivio", "root", "kev20165979");
             AccionesDB acciones = new AccionesDB(conexion);
+            ResultSet rs, rs2;
             String query = "";
-            if(opcion == 1){
-                
-                String correo = request.getParameter("correo");
-                String correo_amigo = request.getParameter("correo_amigo");
-            
-            }else{
-            
+            String respuesta = "";
+            String correo = "";
+            String correo_amigo = "";
+            String id_intercambio = "";
+            switch(opcion){
+                case 1:
+                    correo = request.getParameter("correo");
+                    query = "select usuario.alias, usuario.correo from usuario,amigos where amigos.correo_amigo=usuario.correo and amigos.correo_usuario='"+correo+"'";
+                    rs = acciones.select(query);
+                    while(rs.next()){
+                        respuesta += "<h5>Amigos</h5>"
+                                    +"<label>Alias: "+rs.getString("usuario.alias")+"    Correo: "+rs.getString("usuario.correo")+"</label><br/>";
+                    }
+                    out.print(respuesta);
+                    break;
+                case 2:
+                    correo_amigo = request.getParameter("correo_amigo");
+                    query = "insert into amigos values ('"+correo+"','"+correo_amigo+"')";
+                    acciones.insertDeleteUpdate(query);
+                    correo = request.getParameter("correo");
+                    query = "select usuario.alias, usuario.correo from usuario,amigos where amigos.correo_amigo=usuario.correo and amigos.correo_amigo='"+correo_amigo+"' and amigos.correo_usuario='"+correo+"'";
+                    rs = acciones.select(query);
+                    while(rs.next()){
+                        respuesta = "<label>Alias: "+rs.getString("usuario.alias")+"    Correo: "+rs.getString("usuario.correo")+"</label><br/>";
+                    }
+                    out.print(respuesta);
+                    break;
+                case 3:
+                    correo = request.getParameter("correo");
+                    id_intercambio = request.getParameter("id_intercambio");
+                    query = "select tema.nombre_tema, intercambio.monto_maximo, intercambio.fecha_limite, intercambio.fecha_intercambio, intercambio.comentarios from tema, intercambio where intercambio.id_tema = tema.id and intercambio.id = "+id_intercambio;
+                    rs = acciones.select(query);
+                    while(rs.next()){
+                        respuesta = "<label>Clave del intercambio: "+id_intercambio+"</label><br/>"
+                                  + "<label>Tema: "+rs.getString("tema.nombre_tema")+"</label><br/>"
+                                  +"<label>Monto maximo: $"+rs.getDouble("intercambio.monto_maximo")+"</label><br/>"
+                                  +"<label>Fecha Limite: "+rs.getDate("intercambio.fecha_limite")+"</label><br/>"
+                                  +"<label>Fecha del Intercambio: "+rs.getDate("intercambio.fecha_intercambio")+"</label><br/>"
+                                  +"<label>Comentarios: "+rs.getString("intercambio.comentarios")+"</label><br/>";
+                    }
+                    respuesta += "<button  onclick='aceptar("+id_intercambio+")'>Aceptar</button><br/>"
+                                 +"<button  onclick='rechar()'>Rechazar</button><br/>";
+                    query = "select intercambio.nombre, tema.nombre_tema, intercambio.fecha_intercambio, usuario_intercambio.id_intercambiar "
+                            + "from intercambio, tema, usuario_intercambio where usuario_intercambio.estado_aceptacion=1 and tema.id = intercambio.id_tema and intercambio.id = usuario_intercambio.id_intercambio "
+                            + "and usuario_intercambio.id_usuario = '"+correo+"'";
+                    rs = acciones.select(query);
+                    respuesta += "<h5>Tus Intercambios</h5><br/>";
+                    while(rs.next()){
+                        respuesta += "<div class='acciones'"
+                                    + "<label>Nombre del Intercambio: "+rs.getString("intercambio.nombre")+"</label>"
+                                    +"<label>Tema del Intercambio: "+rs.getString("tema.nombre_tema")+"</label>"
+                                    +"<label>Fecha del Intercambio: "+rs.getDate("intercambio.fecha_intercambio")+"</label>"
+                                    +"<label>Te toco: "+rs.getDate("usuario_intercambio.id_intercambiar")+"</label>"
+                                    +"<div>";
+                    }
+                 
+                    break;
+                case 4:
+                    correo = request.getParameter("correo");
+                    id_intercambio = request.getParameter("id_intercambio");
+                    query = "select estado_aceptacion from usuario_intercambio where id_usuario = '"+correo+"' id_intercambio="+id_intercambio;
+                    rs = acciones.select(query);
+                    if(rs.next()){
+                        query = "update usuario_intercambio set estado_aceptación = 1 where id_usuario = '"+correo+"' id_intercambio="+id_intercambio;
+                        acciones.insertDeleteUpdate(query);
+                    }else{
+                        query = "insert into usuario_intercambio (id_usuario,id_intercambio,estado_aceptacion,creador_intercambio) values('"+correo+"',"+id_intercambio+",true,false)";
+                        acciones.insertDeleteUpdate(query);
+                    }
+                    query = "select id_usuario,id_intercambiar from usuario_intercambio where id_intercambio="+id_intercambio;
+                    rs = acciones.select(query);
+                    ArrayList<String> al = new ArrayList<String>();
+                    while(rs.next()){
+                        al.add(rs.getString("id_usuario"));
+                    }
+                    int temp = (int) (Math.random() * al.size()) + 1;
+                    int temp2 = 0;
+                    while(temp2 < al.size()){
+                        if(temp > al.size()){
+                            temp = 0;
+                        }
+                        query = "update usuario_intercambio set id_intercambiar='"+al.get(temp)+"' where id_usuario='"+al.get(temp2)+"' and id_intercambio="+id_intercambio;
+                        acciones.insertDeleteUpdate(query);
+                        temp2++;
+                        temp++;
+                    }
+                    out.print("Te han agregado al intercambio");
+                    break;
+                default:
+                    break;
             }
         }catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(Inicio.class.getName()).log(Level.SEVERE, null, ex);
--- Intercambio_Regalos/web/Js/acciones.js
+++ Intercambio_Regalos/web/Js/acciones.js
@@ -1,15 +1,23 @@
+var correo = localStorage.getItem("correo");
 $(document).ready(function(){
-    $("#agregar_admigo").click(function(){
+    $("#agregar_amigo").click(function(){
         var agregar = "<div class='acciones'>"
-                       +"<input type='email' id='correo' placeholder='Correo Electronico'focus/>"
+                       +"<input type='email' id='correo_amigo' placeholder='Correo Electronico'focus/>"
                        +"<input type='button' onclick='enviar()' value='Agregar Amigo'/>"
                        +"<input type='button' onclick='cerrar()' value='cerrar'/>"
                        +"<div>";
-        $("#agregar_admigo").parent().append(agregar);
+        $("#agregar_amigo").parent().append(agregar);
+        $.post("AccionesIntercambio",{opcion:1,correo:correo},function(result){
+            $(".acciones").append(result);
+        });
     });
     
     $("#ver_intercambio").click(function(){
-        alert("Ver Intercambios");
+        var agregar = "<div class='acciones'>"
+                       +"<input type='number' id='id_intercambio' placeholder='Id del Intercambio'focus/>"
+                       +"<input type='button' onclick='ver_intercambio()' value='Ver Intercambio'/>"
+                       +"<input type='button' onclick='cerrar()' value='cerrar'/>"
+                       +"<div>";
     });
     
     $("#salir").click(function(){
@@ -19,12 +27,27 @@ $(document).ready(function(){
 });
 
 function enviar(){
-    var correo = $("#correo").val();
-    alert(correo);
+    var correo_amigo = $("#correo_amigo").val();
+    $.post("AccionesIntercambio",{opcion:2,correo:correo,correo_amigo:correo_amigo},function(result){
+        $(".acciones").append(result);
+    });
 }
 
 function cerrar(){
     $(".acciones").remove();
+}
+
+function ver_intercambio(){
+    var id_intercambio = $("id_intercambio").val();
+    $.post("AccionesIntercambio",{opcion:3,id_intercambio:id_intercambio,correo:correo},function(result){
+        $(".acciones").append(result);
+    });
+}
+
+function aceptar(id_intercambio){
+    $.post("AdministrarIntercambio",{opcion:4,id_intercambio,id_intercambio,correo:correo},function(result){
+        
+    });
 }
 
 
